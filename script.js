9b5b40f51382a821cec04bc39d04ce3214d9aeb8
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2023-11-03 14:47:24 +0530

    pending task

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 let graph = document.getElementById('Chart');
 
 let covideRate = {
-    labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Aug'],
+    labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','oct','nov','dec'],
     datasets:[
         {
-            label:'covid-19 affecting rate % in 2020',
+            label:'covid-19 affecting rate in 2020',
             data: [40,50,30,25,60,80,90,50,70,60,55,85],
             borderColor: "red",
             borderWidth: 1,
-            backgroundColor:['blue','green', 'red', 'yellow', 'gray', 'lime', 'orangerd', 'aqua', 'black', 'brown', 'pink', 'white'],
+            backgroundColor:['blue','green', 'lime', 'yellow', 'gray', 'aqua', 'red', 'green', 'white', 'gray', 'pink', 'brown'],
         }
     ]
 };
 
 let chartData = {
-    type:'bar',
+    type:'line',
     data: covideRate ,
     options: {
         responsive: true,
+        scales:{
+            x:{
+                // grid:{
+                // display: true,
+                // },
+                position: 'bottom',
+                backgroundColor:'white',
+                TextColor:'red'
+            },
+            y:{
+                innerWidth:100,
+                backgroundColor:'white',
+                beginAtZero: true,
+            }
+        }
     }
 };
 
